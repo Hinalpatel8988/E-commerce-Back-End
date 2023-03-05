02b0e38a2be7cb84b626359681b698476f8f2611
Hinalpatel8988 <118151523+Hinalpatel8988@users.noreply.github.com>
2023-03-05 17:00:25 +1100

    .env file added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,18 +47,48 @@ router.post('/', async (req, res) => {
       res.status(404).json({ message: 'No Tag with this id!' });
 			return;
 		}
+		res.status(200).json(newTagData);
+	} catch (error) {
+		res.status(400).json(error);
+	}
+});
+
+router.put('/:id', async (req, res) => {
+	// update a tag's name by its `id` value
+	try {
+		const updateTag = await Tag.update(
+			{ tag_name: req.body.tag_name },
+			{
+				where: {
+					id: req.params.id,
+				},
+			});
+		if (!updateTag) {
+			res.status(404).json({ message: 'No Tag found with this id!' });
+			return;
+		}
 		res.status(200).json(data);
 	} catch (error) {
 		res.status(400).json(error);
 	}
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+	const tagtoDelete = await Tag.destroy({
+	   where: {
+		   id: req.params.id,
+	   },
+   });
+   if (!tagtoDelete) {
+	   res.status(404).json({ message: 'No Tag found with that id!' });
+	   return;
+   }
+   res.status(200).json(tagtoDelete);
+} catch (error) {
+   res.status(400).json(error);
+}
 });
 
 module.exports = router;
